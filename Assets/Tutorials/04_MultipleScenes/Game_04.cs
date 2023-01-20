c9ccbff05a8aca52c8dbe623c737d4c8d4d0005f
oldwang-3gold <727281499@qq.com>
2023-01-20 14:00:11 +0800

    第四章起个头

---
+++ Assets/Tutorials/04_MultipleScenes/Game_04.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Game_04 : PersistableObject
+{
+    public ShapeFactory_04 shapeFactory;
+    public PersistentStorage storage;
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode destroyKey = KeyCode.X;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    public float CreationSpeed { get; set; }
+    public float DestructionSpeed { get; set; }
+    float creationProgress, destructionProgress;
+
+    private List<Shape> shapes;
+    const int saveVersion = 1;
+
+    void Awake()
+    {
+        shapes = new List<Shape>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateShape();
+        }
+        else if (Input.GetKeyDown(destroyKey))
+        {
+            DestroyShape();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            storage.Save(this);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+        creationProgress += Time.deltaTime * CreationSpeed;
+        while (creationProgress >= 1f)
+        {
+            creationProgress -= 1f;
+            CreateShape();
+        }
+
+        destructionProgress += Time.deltaTime * DestructionSpeed;
+        while (destructionProgress >= 1f)
+        {
+            destructionProgress -= 1f;
+            DestroyShape();
+        }
+    }
+
+    void CreateShape()
+    {
+        Shape instance = shapeFactory.GetRandom();
+        Transform t = instance.transform;
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        instance.SetColor(Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.25f, 1f, 1f, 1f));
+        shapes.Add(instance);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            shapeFactory.Reclaim(shapes[i]);
+        }
+        shapes.Clear();
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(saveVersion);
+        writer.Write(shapes.Count);
+        for (int i = 0; i < shapes.Count; i++)
+        {
+            //writer.Write(shapes[i].ShapeId);
+            //writer.Write(shapes[i].MaterialId);
+            shapes[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int version = reader.ReadInt();
+        if (version > saveVersion)
+        {
+            Debug.LogError("Unsupported future save version" + version);
+            return;
+        }
+        int count = reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            int shapeId = reader.ReadInt();
+            int materialId = reader.ReadInt();
+            Shape instance = shapeFactory.Get(shapeId, materialId);
+            instance.Load(reader);
+            shapes.Add(instance);
+        }
+    }
+
+    void DestroyShape()
+    {
+        if (shapes.Count > 0)
+        {
+            int index = Random.Range(0, shapes.Count);
+            shapeFactory.Reclaim(shapes[index]);
+            int lastIndex = shapes.Count - 1;
+            shapes[index] = shapes[lastIndex];
+            shapes.RemoveAt(lastIndex);
+        }
+    }
+}

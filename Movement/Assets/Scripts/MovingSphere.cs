fd6f0c4ab85bda7278fd42723e90ac6cd1fd9365
oldwang-3gold <727281499@qq.com>
2023-02-02 15:50:51 +0800

    移动新章节

---
+++ Movement/Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    [SerializeField, Range(0f, 1f)]
+    float bounciness = 0.5f;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        //playerInput.Normalize();
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+        //if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        //{
+        //    //newPosition = transform.localPosition;
+        //    newPosition.x = Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
+        //    newPosition.z = Mathf.Clamp(newPosition.z, allowedArea.xMin, allowedArea.xMax);
+        //}
+        if (newPosition.x < allowedArea.xMin)
+        {
+            newPosition.x = allowedArea.xMin;
+            velocity.x = -velocity.x * bounciness;
+        }
+        else if (newPosition.x > allowedArea.xMax)
+        {
+            newPosition.x = allowedArea.xMax;
+            velocity.x = -velocity.x * bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin)
+        {
+            newPosition.z = allowedArea.yMin;
+            velocity.z = -velocity.z * bounciness;
+        }
+        else if (newPosition.z > allowedArea.yMax)
+        {
+            newPosition.z = allowedArea.yMax;
+            velocity.z = -velocity.z * bounciness;
+        }
+        transform.localPosition = newPosition;
+    }
+}

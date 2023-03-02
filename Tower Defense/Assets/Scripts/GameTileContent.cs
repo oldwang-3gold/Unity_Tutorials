027c9247f8f4af7eb559922995e94bf27dbf2804
oldwang-3gold <727281499@qq.com>
2023-03-02 14:52:04 +0800

    塔防第一章

---
+++ Tower Defense/Assets/Scripts/GameTileContent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameTileContentType
+{
+    Empty, Desination, Wall
+}
+
+public class GameTileContent : MonoBehaviour
+{
+    [SerializeField]
+    GameTileContentType type = default;
+
+    public GameTileContentType Type => type;
+
+    GameTileContentFactory originFactory;
+
+    public GameTileContentFactory OriginFactory
+    {
+        get => originFactory;
+        set
+        {
+            Debug.Assert(originFactory == null, "Redefined origin factory");
+            originFactory = value;
+        }
+    }
+
+    public void Recycle()
+    {
+        originFactory.Reclaim(this);
+    }
+}
---
+++ Tower Defense/Assets/Scripts/GameTileContentFactory.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class GameTileContentFactory : ScriptableObject
+{
+    Scene contentScene;
+
+    [SerializeField]
+    GameTileContent destinationPrefab = default;
+
+    [SerializeField]
+    GameTileContent emptyPrefab = default;
+
+    [SerializeField]
+    GameTileContent wallPrefab = default;
+
+    public void Reclaim (GameTileContent content)
+    {
+        Debug.Assert(content.OriginFactory == this, "Wrong factory reclaimed!");
+        Destroy(content.gameObject);
+    }
+
+    public GameTileContent Get(GameTileContentType type)
+    {
+        switch (type)
+        {
+            case GameTileContentType.Desination: return Get(destinationPrefab);
+            case GameTileContentType.Empty: return Get(emptyPrefab);
+            case GameTileContentType.Wall: return Get(wallPrefab);
+        }
+        Debug.Assert(false, "Unspported type: " + type);
+        return null;
+    }
+
+    GameTileContent Get (GameTileContent prefab)
+    {
+        GameTileContent instance = Instantiate(prefab);
+        instance.OriginFactory = this;
+        MoveToFactoryScene(instance.gameObject);
+        return instance;
+        
+    }
+
+    void MoveToFactoryScene (GameObject o)
+    {
+        if (!contentScene.isLoaded)
+        {
+            if (Application.isEditor)
+            {
+                contentScene = SceneManager.GetSceneByName(name);
+                if (!contentScene.isLoaded)
+                {
+                    contentScene = SceneManager.CreateScene(name);
+                }
+            }
+            else
+            {
+                contentScene = SceneManager.CreateScene(name);
+            }
+        }
+        SceneManager.MoveGameObjectToScene(o, contentScene);
+    }
+}
